11913778f0e7115b860c2f55efd8c25f5d8c1bd9
Tung Nguyen <tongueroo@gmail.com>
2018-02-12 23:53:29 -0800

    fix erb and context definitions, fix bug with empty variables={}

--- lib/render_me_pretty.rb
+++ lib/render_me_pretty.rb
@@ -1,11 +1,13 @@
 require "render_me_pretty/version"
+require "active_support/core_ext/string"
+require "colorize"
 
 module RenderMePretty
   autoload :Erb, 'render_me_pretty/erb'
   autoload :Context, 'render_me_pretty/context'
 
   def self.result(path, variables={})
-    erb = Erb.new(path, variables={})
+    erb = Erb.new(path, variables)
     erb.render
   end
 
