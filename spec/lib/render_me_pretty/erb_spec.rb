9f2dbe1d15a6481a6c68a6f017a7d656842bee4d
Tung Nguyen <tongueroo@gmail.com>
2018-02-12 22:05:15 -0800

    clean up spec

--- spec/lib/render_me_pretty/erb_spec.rb
+++ spec/lib/render_me_pretty/erb_spec.rb
@@ -3,41 +3,33 @@ require 'spec_helper'
 RenderMePretty::Context.load_helpers("./spec/fixtures/helpers")
 
 describe RenderMePretty do
-  context "initial variables" do
-    let(:erb) do
-      RenderMePretty::Erb.new(path, a: 1)
-    end
+  context "valid" do
     let(:path) { "spec/fixtures/valid.erb" }
 
-    context "valid" do
+    context "initial variables" do
+      let(:erb) do
+        RenderMePretty::Erb.new(path, a: 1)
+      end
       it "#render" do
         out = erb.render
         expect(out).to include "a: 1"
       end
     end
-  end
 
-  context "render time variables" do
-    let(:erb) do
-      RenderMePretty::Erb.new(path)
-    end
-    let(:path) { "spec/fixtures/valid.erb" }
-
-    context "valid" do
+    context "render time variables" do
+      let(:erb) do
+        RenderMePretty::Erb.new(path)
+      end
       it "#render" do
         out = erb.render(a: 2)
         expect(out).to include "a: 2"
       end
     end
-  end
 
-  context "both initial and render time variables" do
-    let(:erb) do
-      RenderMePretty::Erb.new(path, a: 3)
-    end
-    let(:path) { "spec/fixtures/valid.erb" }
-
-    context "valid" do
+    context "both initial and render time variables" do
+      let(:erb) do
+        RenderMePretty::Erb.new(path, a: 3)
+      end
       it "#render" do
         out = erb.render(a: 4)
         expect(out).to include "a: 4"
