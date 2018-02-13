884d675b5abfcbeeff0448da7cf0364fc541968b
Tung Nguyen <tongueroo@gmail.com>
2018-02-12 21:47:05 -0800

    passing spec

--- spec/lib/render_me_pretty/erb_spec.rb
+++ spec/lib/render_me_pretty/erb_spec.rb
@@ -3,7 +3,21 @@ require 'spec_helper'
 RenderMePretty::Context.load_helpers("./spec/fixtures/helpers")
 
 describe RenderMePretty do
-  context "erb without initial variables" do
+  context "initial variables" do
+    let(:erb) do
+      RenderMePretty::Erb.new(path, a: 1)
+    end
+    let(:path) { "spec/fixtures/valid.erb" }
+
+    context "valid" do
+      it "#render" do
+        out = erb.render
+        expect(out).to include "a: 1"
+      end
+    end
+  end
+
+  context "render time variables" do
     let(:erb) do
       RenderMePretty::Erb.new(path)
     end
@@ -11,9 +25,22 @@ describe RenderMePretty do
 
     context "valid" do
       it "#render" do
-        out = erb.render
-        puts out
-        expect(out).to include "my_helper value"
+        out = erb.render(a: 2)
+        expect(out).to include "a: 2"
+      end
+    end
+  end
+
+  context "both initial and render time variables" do
+    let(:erb) do
+      RenderMePretty::Erb.new(path, a: 3)
+    end
+    let(:path) { "spec/fixtures/valid.erb" }
+
+    context "valid" do
+      it "#render" do
+        out = erb.render(a: 4)
+        expect(out).to include "a: 4"
       end
     end
   end
