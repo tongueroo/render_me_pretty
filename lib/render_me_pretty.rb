fbd28ab25b1e83a5727c3aab45736f84558f7f2b
Tung Nguyen <tongueroo@gmail.com>
2021-12-28 21:15:11 +0000

    fix activesupport require

--- lib/render_me_pretty.rb
+++ lib/render_me_pretty.rb
@@ -1,6 +1,7 @@
 require "render_me_pretty/version"
+require "active_support"
 require "active_support/core_ext/string"
-require 'rainbow/ext/string'
+require "rainbow/ext/string"
 
 module RenderMePretty
   autoload :Erb, 'render_me_pretty/erb'
